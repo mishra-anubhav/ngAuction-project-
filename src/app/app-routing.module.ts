e18e13c2b7824d9ce2817bd3fc1a20915ae90d68
anubhavmishra1 <39110231+anubhavmishra1@users.noreply.github.com>
2019-06-10 11:22:54 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {ProductDetailComponent} from './product-detail/product-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'products/:productId', component: ProductDetailComponent}
+  { path: 'products/:productId', component: ProductDetailComponent},
+  { path:'login', component:LoginComponent}
 ];
 
 @NgModule({
